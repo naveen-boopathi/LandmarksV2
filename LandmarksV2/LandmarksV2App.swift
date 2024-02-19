052ad33c092604832d5f037eda24cc06c945d58f
Naveen Boopathi <naveenboopathi18@outlook.com>
2024-02-19 08:46:46 +0530

    Initial Commit

---
+++ LandmarksV2/LandmarksV2App.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksV2App.swift
+//  LandmarksV2
+//
+//  Created by Naveen Boopathi on 19/02/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
