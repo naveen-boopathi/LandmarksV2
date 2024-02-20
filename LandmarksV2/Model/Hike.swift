100573a6f3b518b17b032b2d3fafc40abcad4486
Naveen Boopathi <naveenboopathi18@outlook.com>
2024-02-20 11:29:42 +0530

    add animations

---
+++ LandmarksV2/Model/Hike.swift
@@ -0,0 +1,30 @@
+//
+//  Hike.swift
+//  LandmarksV2
+//
+//  Created by Naveen Boopathi on 20/02/24.
+//
+
+import Foundation
+
+struct Hike: Codable, Hashable, Identifiable {
+    var id: Int
+    var name: String
+    var distance: Double
+    var difficulty: Int
+    
+    struct Observation: Codable, Hashable {
+        var distanceFromStart: Double
+        
+        var elevation: Range<Double>
+        var pace: Range<Double>
+        var heartRate: Range<Double>
+    }
+    var observations: [Observation]
+    
+    static var formatter = LengthFormatter()
+    
+    var distanceText: String {
+        Hike.formatter.string(fromValue: distance, unit: .kilometer)
+    }
+}
